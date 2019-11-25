6db561b57ac66924902a60a3efb70b0b1325bfe1
Ty <tyjupiter9@gmail.com>
2019-11-25 17:22:42 -0500

    readded normal+occlusion maps to terrain material, screenshots now save in a screenshot folder in project/game dir

--- Assets/Scripts/DebugKeys.cs
+++ Assets/Scripts/DebugKeys.cs
@@ -26,18 +26,19 @@ public class DebugKeys : MonoBehaviour
     void Screenshot () {
         DateTime date = DateTime.Now;
         int ssnum = 0;
+        string folder = "Screenshots/";
         string filename = date.ToString("MMM d yyyy H mm");
         string extension = $" {ssnum}.png";
         for (int i = 0; i < int.MaxValue; i++) {
-            if (System.IO.File.Exists(filename+extension)) {
+            if (System.IO.File.Exists(folder+filename+extension)) {
                 ssnum++;
                 extension = $" {ssnum}.png";
             } else {
                 break;
             }
         }
-        ScreenCapture.CaptureScreenshot(filename+extension);
-        Debug.Log($"Saved screenshot to {filename+extension}");
+        ScreenCapture.CaptureScreenshot(folder+filename+extension);
+        Debug.Log($"Saved screenshot to {folder+filename+extension}");
     }
 
     void SetMaterialProperty (string property, float value) {
